aefc9ba9a48e205cb2b9aa16307c0b64c9310a3b
Brandon Adams <emidln@gmail.com>
2014-08-29 10:54:53 -0500

    switch to emidln/langdetect fork

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject cld "0.1.2-SNAPSHOT"
+(defproject cld "0.2.0"
   :description "Clojure Language Detection"
   :url "https://github.com/dakrone/cld"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.cybozu.labs/langdetect "1.2-SNAPSHOT"]]
+                 [emidln/langdetect "1.3"]]
   :profiles {:dev {:dependencies [[criterium "0.3.0"]]}}
   :resource-paths ["resources"]
   :warn-on-reflection false)
